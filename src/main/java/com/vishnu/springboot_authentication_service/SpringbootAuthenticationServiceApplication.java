9635eca87b58dd53779cfa2382dc78d134bc52b1
VishnuPrasad-Tech <vp.vishnuprasad117@gmail.com>
2026-08-11 19:45:40 +0530

    Intial Spring Boot Project Setup

---
+++ src/main/java/com/vishnu/springboot_authentication_service/SpringbootAuthenticationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.vishnu.springboot_authentication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthenticationServiceApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/vishnu/springboot_authentication_service/SpringbootAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.springboot_authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
