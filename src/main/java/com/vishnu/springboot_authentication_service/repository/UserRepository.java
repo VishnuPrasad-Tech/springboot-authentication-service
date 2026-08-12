a567d028fc4c3c4ef916aebaa7f39b955e3d7e49
VishnuPrasad-Tech <vp.vishnuprasad117@gmail.com>
2026-08-12 20:52:24 +0530

    user repo done

--- src/main/java/com/vishnu/springboot_authentication_service/repository/UserRepository.java
+++ src/main/java/com/vishnu/springboot_authentication_service/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.vishnu.springboot_authentication_service.repository;
 
-public class UserRepository {
+import com.vishnu.springboot_authentication_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
